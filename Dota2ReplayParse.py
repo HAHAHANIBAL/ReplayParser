c2ae147652e26e963066ce08ae8950fa39053517
Mo Chen <kidrock2006@gmail.com>
2014-01-31 07:45:08 -0500

    Updates
    1. Added player heals stats
    2. Added detailed individual player reports on last hit, denies, damages
    and heals
    3. Added some more attributes on features

--- Dota2ReplayParse.py
+++ Dota2ReplayParse.py
@@ -9,25 +9,30 @@ from collections import OrderedDict
 from Tkinter import *
 from itertools import izip_longest
 
-master = Tk()
+master=Tk()
 master.wm_title("DOTA2 Quant")
 
 photo=PhotoImage(file='DOTA2_header.gif')
 
-canvas = Canvas(master,width=350, height=200)
+canvas=Canvas(master,width=350, height=200)
 canvas.create_image(175,90,image=photo)
 canvas.pack()
-e = Entry(master,width=15)
-w = Label(master, text="Please input your match ID:")
-w.pack()
+e=Entry(master,width=15)
+e2=Entry(master,width=15)
+w=Label(master, text="Please input your match ID:")
+w2=Label(master, text="Please input your desired compile game time:")
 #text=Text(master, width=10,height=10)
 #text.(END,"Please Input Your Match ID:")
 #text.pack()
+w.pack()
 e.pack()
+w2.pack()
+e2.pack()
+e2.focus_set()
 e.focus_set()
 
 
-ordered_fieldnames = OrderedDict([('Match_ID',None),('Radiant_Hero_Kills',None),('Dire_Hero_Kills',None),('Radiant_FB_Bool',None),('Radiant_Creep_Kills',None),('Dire_Creep_Kills',None),('Radiant_Creep_Denies',None),('Dire_Creep_Denies',None),('Smoke_Counts_Radiant',None),('Smoke_Counts_Dire',None),('Glyph_Usage_Radiant',None),('Glyph_Usage_Dire',None),('Radiant_Tower_Damage',None),('Dire_Tower_Damage',None),('Rad_Neutrals_Farmed',None),('Dire_Neutrals_Farmed',None),('Radiant_Ward_Kills',None),('Dire_Ward_Kills',None),('Rad_Hero_1',None),('Rad_Hero_2',None),('Rad_Hero_3',None),('Rad_Hero_4',None),('Rad_Hero_5',None),('Dire_Hero_1',None),('Dire_Hero_2',None),('Dire_Hero_3',None),('Dire_Hero_4',None),('Dire_Hero_5',None)])
+ordered_fieldnames = OrderedDict([('Match_ID',None),('Radiant_Hero_Kills',None),('Dire_Hero_Kills',None),('Radiant_Hero_Damages_Dealt',None),('Dire_Hero_Damages_Dealt',None),('Radiant_Hero_Healed',None),('Dire_Hero_Healed',None),('Radiant_FB_Bool',None),('Radiant_Creep_Kills',None),('Dire_Creep_Kills',None),('Radiant_Creep_Denies',None),('Dire_Creep_Denies',None),('Smoke_Counts_Radiant',None),('Smoke_Counts_Dire',None),('Glyph_Usage_Radiant',None),('Glyph_Usage_Dire',None),('Radiant_Tower_Damage',None),('Dire_Tower_Damage',None),('Rad_Neutrals_Farmed',None),('Dire_Neutrals_Farmed',None),('Radiant_Ward_Kills',None),('Dire_Ward_Kills',None),('Rad_Hero_1',None),('Rad_Hero_2',None),('Rad_Hero_3',None),('Rad_Hero_4',None),('Rad_Hero_5',None),('Dire_Hero_1',None),('Dire_Hero_2',None),('Dire_Hero_3',None),('Dire_Hero_4',None),('Dire_Hero_5',None)])
 fout=open("count.csv","wb")
 stat=csv.DictWriter(fout,fieldnames=ordered_fieldnames)
 stat.writeheader()
@@ -84,16 +89,28 @@ def callback():
 
 #    hero_dire_dict = dict(izip_longest(*[iter(hero_dire_index)] * 2, fillvalue=""))
 #   This is opposite
-    hero_dire_dict={}
-    hero_rad_dict={}
+    hero_dire_dict={};hero_rad_dict={}
+    hero_dire_lh={};hero_rad_lh={}
+    hero_dire_deny={};hero_rad_deny={}
+    hero_dire_heal={};hero_rad_heal={}
+    hero_dire_tango={};hero_rad_tango={}
+    hero_dire_flask={};hero_rad_flask={}
+    hero_dire_bottle={};hero_rad_bottle={}
+
     with open('hero_rad.txt','rb') as fin:
         for line in fin:
             hero_dire_dict[line.strip()]=0
+            hero_dire_lh[line.strip()]=0;hero_dire_deny[line.strip()]=0
+            hero_dire_heal[line.strip()]=0;hero_dire_tango[line.strip()]=0
+            hero_dire_flask[line.strip()]=0;hero_dire_bottle[line.strip()]=0
     fin.close()
 
     with open('hero_dire.txt','rb') as fin:
         for line in fin:
             hero_rad_dict[line.strip()]=0
+            hero_rad_lh[line.strip()]=0;hero_rad_deny[line.strip()]=0
+            hero_rad_heal[line.strip()]=0;hero_rad_tango[line.strip()]=0
+            hero_rad_flask[line.strip()]=0;hero_rad_bottle[line.strip()]=0
     fin.close()
 
 
@@ -133,8 +150,8 @@ def callback():
 #   dire_creep=open('hero_dire.txt','rb')
     count_creep_dire_kills=0
     count_creep_rad_kills=0
-
-
+    count_creep_rad_denies=0
+    count_creep_dire_denies=0
 
     with open("replay_15min.txt","rb") as fin:
         for line in fin:
@@ -149,16 +166,23 @@ def callback():
 #               print str(last_hit_dire.search(line).group(2))
                 if str(last_hit_dire.search(line).group(2)) in hero_list_dire:
 #                   print last_hit_dire.search(line).group(2)
-                    count_creep_dire_kills=count_creep_dire_kills+1
-                count_creep_dire=count_creep_dire+1
+                    count_creep_dire_kills+=1
+                    hero_dire_lh[str(last_hit_dire.search(line).group(2))]+=1
+                elif str(last_hit_dire.search(line).group(2)) in hero_list_rad:
+                    count_creep_rad_denies+=1
+                    hero_rad_deny[str(last_hit_dire.search(line).group(2))]+=1
+                count_creep_dire+=1
             elif "Dire Creep dies" in line:
 #               for item in rad_creep:
                 if str(last_hit_rad.search(line).group(2)) in hero_list_rad:
-                    count_creep_rad_kills=count_creep_rad_kills+1
-                count_creep_rad=count_creep_rad+1
+                    count_creep_rad_kills+=1
+                    hero_rad_lh[str(last_hit_rad.search(line).group(2))]+=1
+                elif str(last_hit_rad.search(line).group(2)) in hero_list_dire:
+                    count_creep_dire_denies+=1
+                    hero_dire_deny[str(last_hit_rad.search(line).group(2))]+=1
+                count_creep_rad+=1
 
-    count_creep_rad_denies=count_creep_dire-count_creep_dire_kills
-    count_creep_dire_denies=count_creep_rad-count_creep_rad_kills
+
     fin.close()
 
 
@@ -220,31 +244,97 @@ def callback():
         first_blood_time=dire_kill_time[0]
         first_blood_rad=0
 
-    hero_dps_pat=re.compile(r'\(\d+:\d+\)..(\w+).*?(\d+).*?to.(\w+).*?')
+    hero_dps_pat=re.compile(r'\(\d+:\d+\)..(\w+).*?deals.(\d+).*?to.(\w+).*?')
+    hero_hps_pat=re.compile(r'\(\d+:\d+\)..(\w+).*?heals.*?(\d+).*?to.(\w+).*?')
+    hero_item_heal_pat=re.compile(r'\(\d+:\d+\)..(\w+).*?gets.*?(\w+).Heal')
+
     with open("replay_15min.txt","rb") as fin:
         for line in fin:
             l=hero_dps_pat.search(line)
+            n=hero_hps_pat.search(line)
+            x=hero_item_heal_pat.search(line)
+            #Damage dealt to enemy heroes
             if l!=None:
                 if l.group(1) in hero_list_dire and l.group(3) in hero_list_rad and 'illusion' not in line:
                     hero_dire_dict[l.group(1)]+=int(l.group(2))
                 elif l.group(1) in hero_list_rad and l.group(3) in hero_list_dire and 'illusion' not in line:
                     hero_rad_dict[l.group(1)]+=int(l.group(2))
+            #Healed by lifesteal,magic wand
+            if n!=None:
+                if n.group(3) in hero_list_dire:
+                    hero_dire_heal[n.group(3)]+=int(n.group(2))
+                elif n.group(3) in hero_list_rad:
+                    hero_rad_heal[n.group(3)]+=int(n.group(2))
+            #Healed By using flask/bottle/tango
+            if x!=None:
+                if x.group(1) in hero_list_dire and x.group(2)=="Tango":
+                    hero_dire_tango[x.group(1)]+=1
+                elif x.group(1) in hero_list_rad and x.group(2)=="Tango":
+                    hero_rad_tango[x.group(1)]+=1
+                elif x.group(1) in hero_list_dire and x.group(2)=="Flask":
+                    hero_dire_flask[x.group(1)]+=1
+                elif x.group(1) in hero_list_rad and x.group(2)=="Flask":
+                    hero_rad_flask[x.group(1)]+=1
+                elif x.group(1) in hero_list_dire and x.group(2)=="Bottle":
+                    hero_dire_bottle[x.group(1)]+=1
+                elif x.group(1) in hero_list_rad and x.group(2)=="Bottle":
+                    hero_rad_bottle[x.group(1)]+=1
     fin.close()
 
-    stat.writerow([matchid,hero_kills_rad,hero_kills_dire,first_blood_rad,count_creep_rad_kills,count_creep_dire_kills,count_creep_rad_denies,count_creep_dire_denies,smoke_counts_rad,smoke_counts_dire,glyph_rad_usage,glyph_dire_usage,count_tower_dmg_rad,count_tower_dmg_dire,neutral_kills_rad,neutral_kills_dire,ward_kills_rad,ward_kills_dire,hero_rad_index[0],hero_rad_index[1],hero_rad_index[2],hero_rad_index[3],hero_rad_index[4],hero_dire_index[0],hero_dire_index[1],hero_dire_index[2],hero_dire_index[3],hero_dire_index[4]])
-    with open('hero_dire_dps.txt','w') as fou:
+    rad_total_hero_damage=0
+    dire_total_hero_damage=0
+
+
+    rad_total_heal=0
+    dire_total_heal=0
+
+    with open('hero_dire_damages.txt','w') as fou:
         for key,val in hero_dire_dict.items():
             key=str(key);val=str(val)
+            dire_total_hero_damage+=int(val)
             fou.write(key+':'+val+'\n')
-    with open('hero_rad_dps.txt','w') as fou:
+    fou.close()
+    with open('hero_rad_damages.txt','w') as fou:
         for key,val in hero_rad_dict.items():
             key=str(key);val=str(val)
+            rad_total_hero_damage+=int(val)
             fou.write(key+':'+val+'\n')
+    fou.close()
+    with open('hero_rad_lh_deny.txt','w') as fou:
+        for key,val in hero_rad_lh.items():
+            key=str(key);val=str(val)
+            fou.write(key+':'+val+"/"+str(hero_rad_deny[key])+'\n')
+    fou.close()
+    with open('hero_dire_lh_deny.txt','w') as fou:
+        for key,val in hero_dire_lh.items():
+            key=str(key);val=str(val)
+            fou.write(key+':'+val+"/"+str(hero_dire_deny[key])+'\n')
+    fou.close()
+    with open('hero_rad_heals.txt','w') as fou:
+        for key,val in hero_rad_heal.items():
+            hero_rad_total_heal=val+hero_rad_tango[key]*115+hero_rad_flask[key]*400+hero_rad_bottle[key]*135
+            rad_total_heal=rad_total_heal+hero_rad_total_heal
+            key=str(key);
+            fou.write(key+':'+str(hero_rad_total_heal)+'\n')
+    fou.close()
+    with open('hero_dire_heals.txt','w') as fou:
+        for key,val in hero_dire_heal.items():
+            hero_dire_total_heal=val+hero_dire_tango[key]*115+hero_dire_flask[key]*400+hero_dire_bottle[key]*135
+            dire_total_heal=dire_total_heal+hero_dire_total_heal
+            key=str(key);
+            fou.write(key+':'+str(hero_dire_total_heal)+'\n')
+    fou.close()
+
+
+
+    stat.writerow([matchid,hero_kills_rad,hero_kills_dire,rad_total_hero_damage,dire_total_hero_damage,rad_total_heal,dire_total_heal,first_blood_rad,count_creep_rad_kills,count_creep_dire_kills,count_creep_rad_denies,count_creep_dire_denies,smoke_counts_rad,smoke_counts_dire,glyph_rad_usage,glyph_dire_usage,count_tower_dmg_rad,count_tower_dmg_dire,neutral_kills_rad,neutral_kills_dire,ward_kills_rad,ward_kills_dire,hero_rad_index[0],hero_rad_index[1],hero_rad_index[2],hero_rad_index[3],hero_rad_index[4],hero_dire_index[0],hero_dire_index[1],hero_dire_index[2],hero_dire_index[3],hero_dire_index[4]])
 #    Generate csv file for dps
 #    w = csv.writer(open("hero_dps.csv", "w"))
 #    for key, val in hero_rad_dict.items():
 #        w.writerow([key, val])
-    fou.close()
+
+
+
 
 def close_window():
     master.destroy()
